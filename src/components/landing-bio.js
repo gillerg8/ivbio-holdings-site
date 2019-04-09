36e326de88c0ae2d15ee10b1d21f568a137f6afc
Geoff Giller <gillerg8@gmail.com>
2019-04-09 07:14:49 -0400

    Update Landing Page

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -1,25 +1,24 @@
-import React from "react"
-import styled from "@emotion/styled"
-import Navbar from "./Navbar.js"
+import React from 'react'
+import styled from '@emotion/styled'
+import Navbar from './Navbar.js'
 
 const Container = styled.div`
-  text-align: center;
+	text-align: center;
 `
 
 const OuterContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-  height: 78vh;
+	position: absolute;
+	left: 50%;
+	top: 40%;
+	transform: translate(-50%, -50%);
 `
 
 const LandingBio = () => (
-  <OuterContainer>
-    <Container>
-      <Navbar />
-    </Container>
-  </OuterContainer>
+	<OuterContainer>
+		<Container>
+			<Navbar />
+		</Container>
+	</OuterContainer>
 )
 
 export default LandingBio
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,37 +5,70 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import styled from "@emotion/styled"
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+import {Link} from 'gatsby'
+import styled from '@emotion/styled'
+import Icon from '../images/ivbio-icon.png'
 
-import "./layout.css"
+import './layout.css'
+
+const Logo = styled(Link)`
+	position: absolute;
+	left: 30px;
+	top: 15px;
+	max-width: 150px;
+	height: 150px;
+	overflow: hidden;
+	align-selft: baseline;
+	img {
+		width: 100%;
+		transition-duration: 0.4s;
+	}
+	&:hover img {
+		transform: scale(1.3);
+	}
+	@media (max-width: 1200px) {
+		display: block;
+		position: relative;
+		left: 0;
+		top: 0;
+	}
+`
 
 const Content = styled.div`
-  margin: 0 auto;
-  max-width: 860px;
-  padding: 0 1.0875rem 1rem;
-  padding-top: 0;
+	margin: 0 auto;
+	max-width: 860px;
+	padding: 0 1.0875rem 1rem;
+	padding-top: 0;
 `
 
 const Footer = styled.footer`
-  display: flex;
-  justify-content: center;
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	padding: 1rem;
+	background-color: #efefef;
+	text-align: center;
 `
 
-const Layout = ({ children }) => (
-  <>
-    <Content>
-      <main>{children}</main>
-      <Footer>
-        © {new Date().getFullYear()} IV BioHoldings. All Rights Reserved.
-      </Footer>
-    </Content>
-  </>
+const Layout = ({children}) => (
+	<Fragment>
+		<Logo to='/'>
+			<img src={Icon} alt='IV Bio Icon' />
+		</Logo>
+		<Content>
+			<main>{children}</main>
+			<Footer>
+				© {new Date().getFullYear()} IV BioHoldings. All Rights Reserved.
+			</Footer>
+		</Content>
+	</Fragment>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired
 }
 
 export default Layout
